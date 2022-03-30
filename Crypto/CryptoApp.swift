5fa3b21fa4238067a2bcbd49abadc322ebdcaccc
Sai Krishna <sai@surveyheart.com>
2022-03-30 10:58:29 +0530

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by sai krishna korukanti on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
